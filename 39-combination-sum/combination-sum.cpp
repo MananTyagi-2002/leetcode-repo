d2c1d25e60d1c73238cf61989b632468a52dc4af
MananTyagi-2002 <91067887+MananTyagi-2002@users.noreply.github.com>
2024-02-10 21:02:38 +0530

    Time: 40 ms (24.02%) | Memory: 42.2 MB (20.33%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,34 +1,29 @@
 class Solution {
 public:
-    void calculatingCombinationSum(vector<int>& candidates, int target,int ind, vector<vector<int>> &ans, vector<int>ds )
+    void helper (vector<int>& candidates, int target, vector<vector<int>>& ans ,int ind, int N, vector<int> set)
     {
-        if(ind==candidates.size())
+        if(ind==N)
         {
             if(target==0)
-         {
-            ans.push_back(ds);
-         }
+            ans.push_back(set);
             return;
         }
+         if(candidates[ind]<=target){
+         //pick
+         set.push_back(candidates[ind]);
+         helper(candidates,target-candidates[ind],ans,ind,N,set);
 
-        if(candidates[ind]<=target)
-        {
-            //picked
-          ds.push_back(candidates[ind]);
-        calculatingCombinationSum( candidates, target-candidates[ind], ind, ans, ds );
-        ds.pop_back();
-        }
-        //not picked
-        calculatingCombinationSum( candidates, target, ind+1, ans, ds );
-        return;
+         //not pick
+         set.pop_back();
+         }
+         helper(candidates,target,ans,ind+1,N,set);
+         return;
+
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> ans;
-        vector<int>ds;
-        int ind=0;
-
-        calculatingCombinationSum( candidates, target, ind, ans, ds );
-        return ans;
-
+        vector<int> set;
+         helper(candidates,target,ans,0,candidates.size(),set);
+         return ans;
     }
 };
